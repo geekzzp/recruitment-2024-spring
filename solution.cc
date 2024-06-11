871a8dc9b334218ac55dc07e645d81160333d4a3
geekzzp <1958230983@qq.com>
2024-06-11 16:33:10 +0800

    v2.2

--- solution.cc
+++ solution.cc
@@ -26,9 +26,7 @@ void optimized_do_phase2(size_t* result, float* data, float* query, size_t size)
     //利用缓存
     #pragma omp parallel for
     for (size_t i = 0; i < size; ++i) {
-        // 使用 std::lower_bound 来寻找不小于 query[i] 的第一个元素的位置
         auto it = std::lower_bound(data, data + size, query[i]);
-        // 由于 it 是指向元素的迭代器，我们需要用 it - data 来获取索引
         result[i] = it - data;
     }
 }
